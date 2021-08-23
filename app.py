191a79a2a0f3a9695efb9069110edba516d8fca4
Acfilok Kolifca <88615645+acfilok96@users.noreply.github.com>
2021-08-23 07:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
+import cv2, os
+import numpy as np
+from keras.models import model_from_json
 
+### json_file = open('C:\\Users\\Dipanakr porey\\Desktop\\Model Deployment\\6th model deployment(dog vs cat)\\cat_vs_dog_model_spare.json', 'r')
+json_file = open("cat_vs_dog_model_spare.json", "r")
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
         
 app = Flask(__name__)
+PATH = "uploads"
 
 @app.route("/")
 def home():
@@ -11,9 +20,19 @@ def home():
 def about():
     if request.method == "POST":
         image_file = request.files["image_file"]
-        path = url_for('static',filename= image_file.filename )
-        
-        return render_template("index.html", result = str(path))
+        path = PATH+"/"+str(image_file.filename)
+        image_file.save(path)
+        secure_path = path
+        img = cv2.imread(path)
+        resized = cv2.resize(img, (500, 500))
+        resized = resized.reshape((1, 500, 500, 3))
+        pred = loaded_model.predict(resized)
+        pred = np.argmax(pred)
+        name_spices = "CAT"
+        os.remove(secure_path)
+        if pred == 1:
+            name_spices = "DOG"
+        return render_template("index.html", result = str(name_spices))
 
 if __name__ == "__main__":
     app.run(debug=True)
